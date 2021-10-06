3b4817dcd3ba5e83f6935166785b6f9054340be4
Kai Jenkins <86726403+KMJENK17@users.noreply.github.com>
2021-10-06 09:39:32 -0400

    finishing server

---
+++ server/app.js
@@ -0,0 +1,62 @@
+// constant sets
+const express = require('express');
+const app = express()
+const session = require('express-session')
+const PORT = process.env.PORT || 9000
+const mustacheExpress = require('mustache-express');
+
+// routers
+const signUpRouter = require('./routes/signup')
+const homeRouter = require('./routes/home')
+
+// global sets
+global.bcrypt = require('bcryptjs')
+global.models = require('./models')
+
+// app sets
+app.use(session({
+    secret: 'ljjojh',
+    resave: false,
+    saveUninitialized: true
+}))
+
+app.use(express.static(__dirname + '/public'));
+app.engine('mustache', mustacheExpress())
+app.set('views', './views')
+app.set('view engine', 'mustache')
+app.use(express.urlencoded())
+
+app.get('/', (req, res) => {
+    res.render('login')
+})
+
+app.use('/signup', signUpRouter)
+app.use('/home', homeRouter)
+
+app.post('/login', (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+
+    models.Users.findOne({ where: { username: username } })
+
+    .then((user) => {
+            bcrypt.compare(password, user.password, function(err, result) {
+                if (result) {
+                    if (req.session) {
+                        req.session.userId = user.id
+                        res.redirect('home')
+                    }
+                } else {
+                    res.render('login', { errorMessage: 'INVALID DETAILS' })
+                }
+            })
+        })
+        .catch((error) => {
+            res.render('login', { errorMessage: "User not found" })
+        })
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+})
---
+++ server/routes/signup.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const router = express()
+const models = require('../models')
+
+router.get('/', (req, res) => {
+    res.render('signup')
+})
+
+router.post('/', (req, res) => {
+    const first_name = req.body.first_name
+    const last_name = req.body.last_name
+    const username = req.body.username
+    const password = req.body.password
+    const email = req.body.email
+    bcrypt.genSalt(10, function(err, salt) {
+        if (!err) {
+            bcrypt.hash(password, salt, function(err, hash) {
+                const user = models.Users.build({
+                    first_name: first_name,
+                    last_name: last_name,
+                    username: username,
+                    password: hash,
+                    email: email
+                })
+                user.save()
+                    .then(newUser => {
+                        console.log('user added successfully')
+                        res.redirect('/')
+                    })
+            })
+        }
+    })
+})
+
+module.exports = router
